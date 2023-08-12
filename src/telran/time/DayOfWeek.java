3afaa0215235af20ff927da1897eed8c150fca55
BorisKoba <browi@mail.ru>
2023-08-12 13:30:51 +0300

    CW 22 Enum intro

---
+++ src/telran/time/DayOfWeek.java
@@ -0,0 +1,34 @@
+package telran.time;
+
+public enum DayOfWeek {
+	MON("Mon#Monday"), TUE("Tue#Tuesday"), WED("Wed#Wednsday"), 
+	THU("Thu#Thursday"), FRI("Fri#Friday"), SAT("Sat#Saturday"), 
+	SUN("Sun#Sunday");
+	String shortName;
+	String fullName;
+	static int N_DAYS = values().length;
+
+	private DayOfWeek(String name) {
+		String[] tokens = name.split("#");
+		shortName = tokens[0];
+		fullName = tokens[1];
+	}
+	public String displayName(TypeDisplay type) {
+		
+		return type == TypeDisplay.SHORT ? shortName : fullName;
+		
+	}
+	public int getValue() {
+		return ordinal() + 1;
+	}
+	public  DayOfWeek plusDays(int days) {
+		int index = (ordinal() + days) % N_DAYS;
+		if(index < 0) {
+			index += N_DAYS;
+		}
+		return values()[index];
+	}
+	public  DayOfWeek minusDays(int days) {
+		return plusDays(-days);
+	}
+}
